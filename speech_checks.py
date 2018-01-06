fa55f41ab5eb3eb1f2a78bffe9944013a8b3db2b
Narcissus <jdefelipe@ucvts.tec.nj.us>
2018-01-06 02:24:14 +0000

    sorted out speech check format

---
+++ speech_checks.py
@@ -0,0 +1,8 @@
+r = input('\x1b[5;35;40m' + "Below is a speech check. The 3 colors represent the success chance you have of passing it. Try your luck right now." + '\x1b[0m' +
+"""
+\x1b[1;32;40m1. 75% success\x1b[0m
+\x1b[1;33;40m2. 50% success\x1b[0m
+\x1b[1;31;40m3. 25% success\x1b[0m
+4. Don't like chance
+
+>>> """)
