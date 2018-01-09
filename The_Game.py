458e2d7eb3b94deb0c83a69f333ab329f621a0e8
Narcissus <jdefelipe@ucvts.tec.nj.us>
2018-01-09 16:18:08 +0000

    Version 1.4: Tutorial almost done, added a way to talk to someone for the second time and bring up a new branch of text, need to add anti-cheating.

--- The_Game.py
+++ The_Game.py
@@ -12,6 +12,12 @@ class Character(object):
 map = ["Tutorial"]
 #will represent the player's accessable areas
 
+acquaintances = []
+#will represent the people the player has already talked to
+
+friends = []
+#will represent the people the player has become friends with
+
 myself = Character("x","Tutorial")
 #this is the player!
 
@@ -118,7 +124,7 @@ print("use the talkto() function to talk to anyone you'd like. to talk to ashley
 #a system message because I think it'd be weird for the characters to talk about these functions directly
 
 ashley = Character("Ashley","Tutorial")
-#creates Ashley so one can talk to her
+#creates Ashley so player can talk to her
 
 #the talkto() function allows for the player to interact with the npc's
 def talkto(character):
@@ -129,152 +135,173 @@ def talkto(character):
 
         #begins Ashley's speech tree
         if character == ashley:
-            print('\x1b[5;35;40m' + f"Hello! Good job, your greeting is quite plain but it does the trick just fine." + '\x1b[0m')
-            time.sleep(s)
-            while True:
-                r = input('\x1b[5;35;40m' + """Anyways, below is a dialogue branch. Type in the number corresponding the response you'd like to give to my statement.""" + '\x1b[0m' +
-                """
-                1. Yes
-                2. No
-                3. Uncertain
-                4. Be rude
+            if ashley in acquaintances:
+                print('\x1b[5;35;40m' + "Hey! Did you check your map yet?" +'\x1b[0m')
+                #prevents user from going through Ashley's speech tree again when map check is required
+            else:
+                acquaintances.append(ashley)
+                print('\x1b[5;35;40m' + f"Hello! Good job, your greeting is quite plain but it does the trick just fine." + '\x1b[0m')
+                time.sleep(s)
+                while True:
+                    r = input('\x1b[5;35;40m' + """Anyways, below is a dialogue branch. Type in the number corresponding the response you'd like to give to my statement.""" + '\x1b[0m' +
+                    """
+                    1. Yes
+                    2. No
+                    3. Uncertain
+                    4. Be rude
 
-                >>> """)
+                    >>> """)
 
-                if r == "1":
-                    print('\x1b[1;37;40m' + "So, like this, right?" + '\x1b[0m')
-                    time.sleep(s)
-                    print('\x1b[5;35;40m' + f"Yes! That's perfect, {myself.name}. Easy, right?" + '\x1b[0m')
-                    break
+                    if r == "1":
+                        print('\x1b[1;37;40m' + "So, like this, right?" + '\x1b[0m')
+                        time.sleep(s)
+                        print('\x1b[5;35;40m' + f"Yes! That's perfect, {myself.name}. Easy, right?" + '\x1b[0m')
+                        break
 
-                elif r == "2":
-                    print('\x1b[1;37;40m' + "I don't really want to." + '\x1b[0m')
-                    time.sleep(s)
-                    print('\x1b[5;35;40m' + "Well even if you didn't, you responded just now." + '\x1b[0m')
-                    break
+                    elif r == "2":
+                        print('\x1b[1;37;40m' + "I don't really want to." + '\x1b[0m')
+                        time.sleep(s)
+                        print('\x1b[5;35;40m' + "Well even if you didn't, you responded just now." + '\x1b[0m')
+                        break
 
-                elif r == "3":
-                    print('\x1b[1;37;40m' + "I'm not too sure how to do this" + '\x1b[0m')
-                    time.sleep(s)
-                    print('\x1b[5;35;40m' + "It's alright! You did it just now. Isn't it easy?" + '\x1b[0m')
-                    break
+                    elif r == "3":
+                        print('\x1b[1;37;40m' + "I'm not too sure how to do this" + '\x1b[0m')
+                        time.sleep(s)
+                        print('\x1b[5;35;40m' + "It's alright! You did it just now. Isn't it easy?" + '\x1b[0m')
+                        break
 
-                elif r == "4":
-                    print('\x1b[1;37;40m' + "I don't need this stupid tutorial. I know how to talk to people." + '\x1b[0m')
-                    time.sleep(s)
-                    print('\x1b[5;35;40m' + "..." + '\x1b[0m')
-                    time.sleep(1)
-                    print('\x1b[5;35;40m' + "..." + '\x1b[0m')
-                    time.sleep(1)
-                    print('\x1b[5;35;40m' + "Fine! If you think you can make friends without my help go on right ahead!" + '\x1b[0m')
-                    time.sleep(s)
-                    while True:
-                        r = input("""Ashley storms off and leaves you alone in the Tutorial room. what would you like to do?
-                        1. Go into the dating simulator without her help.
-                        2. Try to win back her forgiveness.
+                    elif r == "4":
+                        print('\x1b[1;37;40m' + "I don't need this stupid tutorial. I know how to talk to people." + '\x1b[0m')
+                        time.sleep(s)
+                        print('\x1b[5;35;40m' + "..." + '\x1b[0m')
+                        time.sleep(1)
+                        print('\x1b[5;35;40m' + "..." + '\x1b[0m')
+                        time.sleep(1)
+                        print('\x1b[5;35;40m' + "Fine! If you think you can make friends without my help go on right ahead!" + '\x1b[0m')
+                        time.sleep(s)
+                        while True:
+                            r = input("""Ashley storms off and leaves you alone in the Tutorial room. what would you like to do?
+                            1. Go into the dating simulator without her help.
+                            2. Try to win back her forgiveness.
 
-                        >>> """)
+                            >>> """)
 
-                        if r == "1":
-                            print('\x1b[1;37;40m' + "Finally, let's start the game already." + '\x1b[0m')
-                            time.sleep(s)
-                            print("you leave the room and shield your eyes as the sun beams down.")
-                            time.sleep(s)
-                            print("once your eyes adjust to the light, you see you're now in a park.")
-                            time.sleep(s)
-                            print("you turn around and realize the tutorial room was just a huge playground.")
-                            myself.location = "Joe Collins Park"
-                            map.append("Joe Collins Park")
-                            print("'Joe Collins Park' has been added to your map!")
-                            time.sleep(s)
-                            print("type in 'map' (no quotes) to see your list of unlocked locations.")
-                            time.sleep(s)
-                            print("by the way, i'm the narrator! i'll be with you for the entire game.")
-                            time.sleep(s)
-                            print("do talkto(narrator) if you have any questions. you did skip the tutorial after all.")
-                            time.sleep(s)
-                            print("well, you can do as you please now. enjoy the game!")
-                            time.sleep(s)
+                            if r == "1":
+                                print('\x1b[1;37;40m' + "Finally, let's start the game already." + '\x1b[0m')
+                                time.sleep(s)
+                                print("you leave the room and shield your eyes as the sun beams down.")
+                                time.sleep(s)
+                                print("once your eyes adjust to the light, you see you're now in a park.")
+                                time.sleep(s)
+                                print("you turn around and realize the tutorial room was just a huge playground.")
+                                myself.location = "Joe Collins Park"
+                                map.append("Joe Collins Park")
+                                print("'Joe Collins Park' has been added to your map!")
+                                time.sleep(s)
+                                print("type in 'map' (no quotes) to see your list of unlocked locations.")
+                                time.sleep(s)
+                                print("by the way, i'm the narrator! i'll be with you for the entire game.")
+                                time.sleep(s)
+                                print("do talkto(narrator) if you have any questions. you did skip the tutorial after all.")
+                                time.sleep(s)
+                                print("well, you can do as you please now. enjoy the game!")
+                                time.sleep(s)
+                                break
                             break
                         break
-                    break
-
-                else:
-                    print('\x1b[5;35;40m' + "Type in one of the given numbers, I can't understand you." +'\x1b[0m')
-
-            time.sleep(s)
-
-            while myself.location=="Tutorial":
-
-                r = input('\x1b[5;35;40m' + "Below is a speech check. The 3 colors represent the success chance you have of passing it. Try your luck right now." + '\x1b[0m' +
-                """
-                \x1b[1;32;40m1. 75% success\x1b[0m
-                \x1b[1;33;40m2. 50% success\x1b[0m
-                \x1b[1;31;40m3. 25% success\x1b[0m
-                4. Don't like chance
-
-                >>> """)
-
-                time.sleep(s)
-
-                if r == "1":
-                    print('\x1b[1;37;40m' + "I wouldn't say I'm very charismatic, but I think I can talk just fine." + '\x1b[0m')
-                    time.sleep(s)
-                    k=random.randint(1,4)
-                    #creates a random number
-
-                    if k == 1:
-                    #25% chance to get a 1; 25% chance to fail check
-                        print('\x1b[5;35;40m' + "Yeah you really are a lousy speaker. Oh well, can't be helped." + '\x1b[0m')
-                        time.sleep(s)
 
                     else:
-                    #75% not to get a 1; 75% chance to pass check
-                        print('\x1b[5;35;40m' + "Don't be silly; I think you have a lot of charisma!" + '\x1b[0m')
-                        time.sleep(s)
+                        print('\x1b[5;35;40m' + "Type in one of the given numbers, I can't understand you." +'\x1b[0m')
 
-                    break
-
-                elif r == "2":
-                    print('\x1b[1;37;40m' + "I think I can talk to people just fine." + '\x1b[0m')
                     time.sleep(s)
-                    k=random.randint(1,2)
 
-                    if k == 1:
-                        print('\x1b[5;35;40m' + "Uhh... think again. You might need to work on that." + '\x1b[0m')
+                while myself.location=="Tutorial":
+
+                    r = input('\x1b[5;35;40m' + "Below is a speech check. The 3 colors represent the success chance you have of passing it. Try your luck right now." + '\x1b[0m' +
+                    """
+                    \x1b[1;32;40m1. 75% success\x1b[0m
+                    \x1b[1;33;40m2. 50% success\x1b[0m
+                    \x1b[1;31;40m3. 25% success\x1b[0m
+                    4. Don't like chance
+
+                    >>> """)
+
+                    time.sleep(s)
+
+                    if r == "1":
+                        print('\x1b[1;37;40m' + "I wouldn't say I'm very charismatic, but I think I can talk just fine." + '\x1b[0m')
                         time.sleep(s)
+                        k=random.randint(1,4)
+                        #creates a random number
+
+                        if k == 1:
+                        #25% chance to get a 1; 25% chance to fail check
+                            print('\x1b[5;35;40m' + "Yeah you really are a lousy speaker. Oh well, can't be helped." + '\x1b[0m')
+                            time.sleep(s)
+
+                        else:
+                        #75% not to get a 1; 75% chance to pass check
+                            print('\x1b[5;35;40m' + "Don't be silly; I think you have a lot of charisma!" + '\x1b[0m')
+                            time.sleep(s)
+
+                        break
+
+                    elif r == "2":
+                        print('\x1b[1;37;40m' + "I think I can talk to people just fine." + '\x1b[0m')
+                        time.sleep(s)
+                        k=random.randint(1,2)
+
+                        if k == 1:
+                            print('\x1b[5;35;40m' + "Uhh... think again. You might need to work on that." + '\x1b[0m')
+                            time.sleep(s)
+
+                        else:
+                            print('\x1b[5;35;40m' + "You certainly can! I think you'll be able to make friends pretty easily." + '\x1b[0m')
+                            time.sleep(s)
+
+                        break
+
+                    elif r == "3":
+                        print('\x1b[1;37;40m' + "I'm an amazing speaker, I can make friends with no problem." + '\x1b[0m')
+                        time.sleep(s)
+                        k=random.randint(1,4)
+
+                        if k == 1:
+                            print('\x1b[5;35;40m' + "Wow, I can see where you're coming from. You're pretty confident too." + '\x1b[0m')
+                            time.sleep(s)
+
+                        else:
+                            print('\x1b[5;35;40m' + "Um, I don't think so. You might need to tone it down a bit." + '\x1b[0m')
+                            time.sleep(s)
+
+                        break
+
+                    elif r == "4":
+                        print('\x1b[1;37;40m' + "I don't know; charisma isn't really my specialty." + '\x1b[0m')
+                        time.sleep(s)
+                        print('\x1b[5;35;40m' + "Some people are a little more shy than others. That's fine." + '\x1b[0m')
+                        break
 
                     else:
-                        print('\x1b[5;35;40m' + "You certainly can! I think you'll be able to make friends pretty easily." + '\x1b[0m')
+                    #prevents user from typing something not fitting
+                        print('\x1b[5;35;40m' + "I'm sorry, I don't know what you're trying to say." + '\x1b[0m')
                         time.sleep(s)
 
-                    break
+                if myself.location == "Tutorial":
 
-                elif r == "3":
-                    print('\x1b[1;37;40m' + "I'm an amazing speaker, I can make friends with no problem." + '\x1b[0m')
+                    print('\x1b[5;35;40m' + "You've been doing a great job so far!" +'\x1b[0m')
                     time.sleep(s)
-                    k=random.randint(1,4)
-
-                    if k == 1:
-                        print('\x1b[5;35;40m' + "Wow, I can see where you're coming from. You're pretty confident too." + '\x1b[0m')
-                        time.sleep(s)
-
-                    else:
-                        print('\x1b[5;35;40m' + "Um, I don't think so. You might need to tone it down a bit." + '\x1b[0m')
-                        time.sleep(s)
-
-                    break
-
-                elif r == "4":
-                    print('\x1b[1;37;40m' + "I don't know; charisma isn't really my specialty." + '\x1b[0m')
+                    print('\x1b[5;35;40m' + "I only have a couple of quick tips to show you before I send you on your way." +'\x1b[0m')
                     time.sleep(s)
-                    print('\x1b[5;35;40m' + "Some people are a little more shy than others. That's fine." + '\x1b[0m')
-                    break
-
-                else:
-                #prevents user from typing something not fitting
-                    print('\x1b[5;35;40m' + "I'm sorry, I don't know what you're trying to say." + '\x1b[0m')
+                    print('\x1b[5;35;40m' + "The game has several locations you can visit." +'\x1b[0m')
                     time.sleep(s)
+                    print('\x1b[5;35;40m' + "Here's a map. Take a look at it right now." +'\x1b[0m')
+                    time.sleep(s)
+                    print("to look at your map, type in 'map' (without quotes) to see a list of discovered locations.")
+
+
+
+
 
 
     else:
