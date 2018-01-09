7b79c8b382b562b26a007041d4039cea619dd1f4
Narcissus <jdefelipe@ucvts.tec.nj.us>
2018-01-09 23:54:45 +0000

    instead of using talkto() directly, will say 'hello' to prevent user from saying anything else

--- cheating_prevention_test.py
+++ cheating_prevention_test.py
@@ -1,4 +1,4 @@
-map = ["Tutorial", "The Park", "The Cafe"]
+"""map = ["Tutorial", "The Park", "The Cafe"]
 friends = ["myself"]
 
 while True:
@@ -12,6 +12,11 @@ while True:
         print(map)
 
     elif a == "friends":
-        print(friends)
+        print(friends)"""
+
+while True:
+    a=input(">>> ").lower()
+    if a == f"hello {character}":
+        print('\x1b[1;37;40m' + "Hello, {character}!" + '\x1b[0m')
 
 
