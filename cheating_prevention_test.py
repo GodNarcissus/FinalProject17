27b04e64d293530fb991669799152a87c0eec543
Narcissus <jdefelipe@ucvts.tec.nj.us>
2018-01-09 16:15:51 +0000

    tested out a way to have the user only affect the game by typing allowed words.

---
+++ cheating_prevention_test.py
@@ -0,0 +1,17 @@
+map = ["Tutorial", "The Park", "The Cafe"]
+friends = ["myself"]
+
+while True:
+    a = input(">>> ")
+    #the user's "command line"
+
+    if a == "talk to ___":
+        print("insert branch")
+
+    elif a == "map":
+        print(map)
+
+    elif a == "friends":
+        print(friends)
+
+
